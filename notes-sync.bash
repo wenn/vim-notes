d6b5e62c5905700629f1d89c38ca07ea87b09fff
Nguyen Nguyen <nguyen.nguyen@capitalone.com>
2016-07-28 21:42:23 -0500

    add note sync to run in background

--- notes-sync.bash
+++ notes-sync.bash
@@ -7,15 +7,15 @@ source $NOTES_ROOT/.config
 cd $NOTES_ROOT
 if [[ ! -z $NOTES_GIT_URL ]]; then
     if [[ -z $(git remote -vv) ]]; then
-        git remote add origin $NOTES_GIT_URL 2>>$NOTES_ROOT/.error_log
-        git fetch origin  2>>$NOTES_ROOT/.error_log
+        git remote add origin $NOTES_GIT_URL 2>>$NOTES_ROOT/.error_log 1>&2
+        git fetch origin  2>>$NOTES_ROOT/.error_log 1>&2
     fi
 
     (
-        git fetch origin 2>>$NOTES_ROOT/.error_log
-        git add -A 2>>$NOTES_ROOT/.error_log
-        git commit -m "installation commit" 2>>$NOTES_ROOT/.error_log
-        git rebase origin/master 2>>$NOTES_ROOT/.error_log
-        git push origin master 2>>$NOTES_ROOT/.error_log
-    )
+        git fetch origin 2>>$NOTES_ROOT/.error_log 1>&2 && \
+        git add -A 2>>$NOTES_ROOT/.error_log 1>&2 && \
+        git commit -m "installation commit" 2>>$NOTES_ROOT/.error_log 1>&2 && \
+        git rebase origin/master 2>>$NOTES_ROOT/.error_log 1>&2 && \
+        git push origin master 2>>$NOTES_ROOT/.error_log 1>&2
+    ) &
 fi
