09c54869fc36e37c9ad28a37b3c1e1d87c4a7774
Nguyen Nguyen <nguyen.nguyen@capitalone.com>
2016-07-28 22:48:49 -0500

    update sync to push only if there are changes

--- notes-sync.bash
+++ notes-sync.bash
@@ -11,11 +11,19 @@ if [[ ! -z $NOTES_GIT_URL ]]; then
         git fetch origin  2>>$NOTES_ROOT/.error_log 1>&2
     fi
 
+    has_changed=$({ git diff --name-only ; \
+        git diff --name-only --staged ; \
+        git ls-files --other --exclude-standard ; \
+      } | sort | uniq)
+
     (
         git fetch 2>>$NOTES_ROOT/.error_log 1>&2 && \
-        git merge origin/master 2>>$NOTES_ROOT/.error_log 1>&2 && \
-        git add -A 2>>$NOTES_ROOT/.error_log 1>&2 && \
-        git commit -m "Syncing..." 2>>$NOTES_ROOT/.error_log 1>&2 && \
-        git push origin master 2>>$NOTES_ROOT/.error_log 1>&2
+        git rebase origin/master 2>>$NOTES_ROOT/.error_log 1>&2 && \
+
+        if [[ ! -z $has_changed ]]; then
+          git add -A 2>>$NOTES_ROOT/.error_log 1>&2 && \
+          git commit -m "Syncing..." 2>>$NOTES_ROOT/.error_log 1>&2 && \
+          git push origin master 2>>$NOTES_ROOT/.error_log 1>&2
+        fi
     ) &
 fi
