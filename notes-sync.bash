840da82cd744db4738cc669d2182ced27fcb1659
Nguyen Nguyen <nguyen.nguyen@capitalone.com>
2016-07-28 22:29:01 -0500

    pruning bad objects

--- notes-sync.bash
+++ notes-sync.bash
@@ -12,10 +12,12 @@ if [[ ! -z $NOTES_GIT_URL ]]; then
     fi
 
     (
-        git fetch origin 2>>$NOTES_ROOT/.error_log 1>&2 && \
+        git gc --prune=now 2>>$NOTES_ROOT/.error_log 1>&2 && \
+        git remote prune origin 2>>$NOTES_ROOT/.error_log 1>&2 && \
+        git fetch 2>>$NOTES_ROOT/.error_log 1>&2 && \
         git add -A 2>>$NOTES_ROOT/.error_log 1>&2 && \
         git commit -m "Syncing..." 2>>$NOTES_ROOT/.error_log 1>&2 && \
         git rebase origin/master 2>>$NOTES_ROOT/.error_log 1>&2 && \
-        git push -f origin master 2>>$NOTES_ROOT/.error_log 1>&2
+        git push origin master 2>>$NOTES_ROOT/.error_log 1>&2
     ) &
 fi
